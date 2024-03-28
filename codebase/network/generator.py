c866a27dcac5528c6ea6d0f8a96dc99f8e57b66a
Felipe Perez <perezmaf@myumanitoba.ca>
2024-03-28 04:48:14 -0500

    Clean up code

--- codebase/network/generator.py
+++ codebase/network/generator.py
@@ -1,23 +1,22 @@
 import torch
 from torch import nn
 
+
 class get_gernerator(nn.Module):
     def __init__(self):
         super(get_gernerator, self).__init__()
-        w = torch.ones([256*256*256, 3], requires_grad=True)
-        b = torch.zeros([256*256*256, 3], requires_grad=True)
+        w = torch.ones([256 * 256 * 256, 3], requires_grad=True)
+        b = torch.zeros([256 * 256 * 256, 3], requires_grad=True)
         self.w = torch.nn.Parameter(w)
         self.b = torch.nn.Parameter(b)
         self.register_parameter("weight_trans", self.w)
         self.register_parameter("bias_trans", self.b)
 
-
-    def forward(self,img):
-
+    def forward(self, img):
         img = torch.transpose(img, 1, 3)
         img = torch.transpose(img, 1, 2)
 
-        idx = img[:, :, :, 0] * 256*256 + img[:, :, :, 1] * 256 + img[:, :, :, 2]
+        idx = img[:, :, :, 0] * 256 * 256 + img[:, :, :, 1] * 256 + img[:, :, :, 2]
 
         idx = idx.long()
 

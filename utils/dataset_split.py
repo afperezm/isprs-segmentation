bd1b3d1c780c9838c96582552adb9eda9b22a17a
Felipe Perez <perezmaf@myumanitoba.ca>
2024-04-02 17:10:03 -0500

    Condition imaging mode

--- utils/dataset_split.py
+++ utils/dataset_split.py
@@ -7,7 +7,8 @@ import progressbar
 import tifffile as tiff
 
 
-IMAGES_DIR = '2_Ortho_RGB'
+IMAGES_RGB_DIR = '2_Ortho_RGB'
+IMAGES_IR_DIR = '3_Ortho_IRRG'
 LABELS_DIR = '5_Labels_all'
 
 
@@ -56,13 +57,23 @@ def spit_image_and_label(images_dir, labels_dir, image_path, label_path, patch_s
 def main():
     data_dir = PARAMS.data_dir
     output_dir = PARAMS.output_dir
+    use_ir = PARAMS.use_ir
     patch_size = PARAMS.patch_size
-    stride = PARAMS.stride_size
+    stride = PARAMS.stride
 
-    images_dir = os.path.join(data_dir, IMAGES_DIR)
+    if use_ir:
+        images_dir = os.path.join(data_dir, IMAGES_IR_DIR)
+    else:
+        images_dir = os.path.join(data_dir, IMAGES_RGB_DIR)
+
     labels_dir = os.path.join(data_dir, LABELS_DIR)
 
-    output_images_dir = os.path.join(output_dir, f'images_{patch_size}_{stride}')
+    if use_ir:
+        mode = "irrg"
+    else:
+        mode = "rgb"
+
+    output_images_dir = os.path.join(output_dir, f'images_{mode}_{patch_size}_{stride}')
     output_labels_dir = os.path.join(output_dir, f'labels_{patch_size}_{stride}')
 
     if not os.path.exists(output_images_dir):
@@ -90,6 +101,7 @@ def parse_args():
     parser = argparse.ArgumentParser("Spitter of ISPRS datasets into small patches")
     parser.add_argument("--data_dir", required=True)
     parser.add_argument("--output_dir", required=True)
+    parser.add_argument("--use_ir", action="store_true")
     parser.add_argument("--patch_size", type=int, default=512)
     parser.add_argument("--stride", type=int, default=256)
     return parser.parse_args()

b06985963dab20b142387cd0ce9f30f296fcf94e
Felipe Perez <perezmaf@myumanitoba.ca>
2024-04-15 21:28:36 -0500

    Allow to optionally cropping instead of padding

--- utils/dataset_split.py
+++ utils/dataset_split.py
@@ -31,7 +31,19 @@ def pad_image(image, patch_size):
     return image_padded
 
 
-def crop_image_and_label(output_dir, image_path, label_path, patch_size, stride, scale):
+def crop_image(image, patch_size):
+
+    original_height, original_width = image.shape[0], image.shape[1]
+
+    extent_height = int(original_height / patch_size) * patch_size
+    extent_width = int(original_width / patch_size) * patch_size
+
+    image_cropped = image[0:extent_height, 0:extent_width]
+
+    return image_cropped
+
+
+def crop_image_and_label(output_dir, image_path, label_path, patch_size, stride, scale, crop):
 
     images_dir = os.path.join(output_dir, f'images')
     labels_dir = os.path.join(output_dir, f'labels')
@@ -51,8 +63,12 @@ def crop_image_and_label(output_dir, image_path, label_path, patch_size, stride,
     image_rescaled = rescale_image(image, scale)
     label_rescaled = rescale_image(label, scale)
 
-    image_padded = pad_image(image_rescaled, patch_size)
-    label_padded = pad_image(label_rescaled, patch_size)
+    if crop:
+        image_padded = crop_image(image_rescaled, patch_size)
+        label_padded = crop_image(label_rescaled, patch_size)
+    else:
+        image_padded = pad_image(image_rescaled, patch_size)
+        label_padded = pad_image(label_rescaled, patch_size)
 
     assert image_padded.shape == label_padded.shape
 
@@ -82,6 +98,7 @@ def main():
     stride = PARAMS.stride
     scale = PARAMS.scale
     seed = PARAMS.seed
+    crop = PARAMS.crop
 
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
@@ -104,7 +121,7 @@ def main():
 
     bar = progressbar.ProgressBar(max_value=num_train_images)
     for idx, (img_path, msk_path) in enumerate(zip(train_images_paths, train_labels_paths)):
-        crop_image_and_label(os.path.join(output_dir, 'train'), img_path, msk_path, patch_size, stride, scale)
+        crop_image_and_label(os.path.join(output_dir, 'train'), img_path, msk_path, patch_size, stride, scale, crop)
         bar.update(idx)
     bar.update(num_train_images)
 
@@ -112,7 +129,7 @@ def main():
 
     bar = progressbar.ProgressBar(max_value=num_test_images)
     for idx, (img_path, msk_path) in enumerate(zip(test_images_paths, test_labels_paths)):
-        crop_image_and_label(os.path.join(output_dir, 'test'), img_path, msk_path, patch_size, stride, scale)
+        crop_image_and_label(os.path.join(output_dir, 'test'), img_path, msk_path, patch_size, stride, scale, crop)
         bar.update(idx)
     bar.update(num_test_images)
 
@@ -126,6 +143,7 @@ def parse_args():
     parser.add_argument("--stride", type=int, default=256)
     parser.add_argument("--scale", type=float, default=1.0)
     parser.add_argument("--seed", type=int, default=42)
+    parser.add_argument("--crop", help="Flag to indicate cropping instead of padding", action="store_true")
     return parser.parse_args()
 
 

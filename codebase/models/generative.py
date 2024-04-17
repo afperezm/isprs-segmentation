0d13cdfe0f4de2fa7a140e271aff4505afbecfee
Felipe Perez <perezmaf@myumanitoba.ca>
2024-04-17 11:30:54 -0500

    Generate fake source images during generator training

--- codebase/models/generative.py
+++ codebase/models/generative.py
@@ -24,26 +24,10 @@ class ColorMapGAN(LightningModule):
 
         optimizer_g, optimizer_d = self.optimizers()
 
-        # Generate images
-        fake_source_images = self.generator(target_images)
-
-        # Log images
-        if self.global_step % 1000 == 0:
-            grid = torchvision.utils.make_grid(source_images, normalize=True, value_range=(0, 255))
-            self.logger.experiment.add_image(tag="train/source_images", img_tensor=grid,
-                                             global_step=int(self.global_step / 1000) % 5)
-
-            grid = torchvision.utils.make_grid(target_images, normalize=True, value_range=(0, 255))
-            self.logger.experiment.add_image(tag="train/target_images", img_tensor=grid,
-                                             global_step=int(self.global_step / 1000) % 5)
-
-            grid = torchvision.utils.make_grid(fake_source_images, normalize=True, value_range=(0, 255))
-            self.logger.experiment.add_image(tag="train/fake_source_images", img_tensor=grid,
-                                             global_step=int(self.global_step / 1000) % 5)
-
         # Train generator
         self.toggle_optimizer(optimizer_g)
         optimizer_g.zero_grad()
+        fake_source_images = self.generator(target_images)
         # valid = torch.ones(source_images.size(0), 1, 8, 8).type_as(source_images)
         pred_fake_source_images = self.discriminator(fake_source_images)
         g_loss = self.mse_loss(pred_fake_source_images, torch.ones_like(pred_fake_source_images))
@@ -60,12 +44,26 @@ class ColorMapGAN(LightningModule):
         real_loss = self.mse_loss(pred_real_source_images, torch.zeros_like(pred_real_source_images))
         # # how well can it label as fake?
         # fake = torch.zeros(source_images.size(0), 1, 8, 8).type_as(source_images)
-        pred_fake_source_images = self.discriminator(fake_source_images)
+        pred_fake_source_images = self.discriminator(fake_source_images.detach())
         fake_loss = self.mse_loss(pred_fake_source_images, torch.zeros_like(pred_fake_source_images))
         d_loss = 0.5 * (real_loss + fake_loss)
         self.manual_backward(d_loss)
         optimizer_d.step()
         self.untoggle_optimizer(optimizer_d)
+
+        # Log images
+        if self.global_step % 1000 == 0:
+            grid = torchvision.utils.make_grid(source_images, normalize=True, value_range=(0, 255))
+            self.logger.experiment.add_image(tag="train/source_images", img_tensor=grid,
+                                             global_step=int(self.global_step / 1000) % 5)
+
+            grid = torchvision.utils.make_grid(target_images, normalize=True, value_range=(0, 255))
+            self.logger.experiment.add_image(tag="train/target_images", img_tensor=grid,
+                                             global_step=int(self.global_step / 1000) % 5)
+
+            grid = torchvision.utils.make_grid(fake_source_images, normalize=True, value_range=(0, 255))
+            self.logger.experiment.add_image(tag="train/fake_source_images", img_tensor=grid,
+                                             global_step=int(self.global_step / 1000) % 5)
 
         self.log("train/g_loss", g_loss, prog_bar=True)
         self.log("train/d_loss", d_loss, prog_bar=True)

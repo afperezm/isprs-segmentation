c61b24d6c52c86d6f2fd78f814a9597028af7e92
Felipe Perez <perezmaf@myumanitoba.ca>
2024-04-17 03:24:04 -0500

    Apply instance normalization on PatchGAN discriminator

--- codebase/networks/discriminator.py
+++ codebase/networks/discriminator.py
@@ -10,10 +10,13 @@ class Discriminator(nn.Module):
             nn.Conv2d(in_channels=num_channels, out_channels=num_features, kernel_size=4, stride=2, padding=1),
             nn.LeakyReLU(negative_slope=0.2, inplace=True),
             nn.Conv2d(in_channels=num_features, out_channels=num_features * 2, kernel_size=4, stride=2, padding=1),
+            nn.InstanceNorm2d(num_features * 2),
             nn.LeakyReLU(negative_slope=0.2, inplace=True),
             nn.Conv2d(in_channels=num_features * 2, out_channels=num_features * 4, kernel_size=4, stride=2, padding=1),
+            nn.InstanceNorm2d(num_features * 4),
             nn.LeakyReLU(negative_slope=0.2, inplace=True),
             nn.Conv2d(in_channels=num_features * 4, out_channels=num_features * 8, kernel_size=4, stride=2, padding=1),
+            nn.InstanceNorm2d(num_features * 8),
             nn.LeakyReLU(negative_slope=0.2, inplace=True),
             nn.Conv2d(in_channels=num_features * 8, out_channels=1, kernel_size=4, stride=2, padding=1),
         )

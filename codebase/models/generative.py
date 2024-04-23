5a35df51c3289cd326d140483ba770a6ef69f8e8
Felipe Perez <perezmaf@myumanitoba.ca>
2024-04-23 02:48:40 -0500

    Fix training cycle of ColorMapGAN

--- codebase/models/generative.py
+++ codebase/models/generative.py
@@ -26,33 +26,29 @@ class ColorMapGAN(pl.LightningModule):
 
         optimizer_g, optimizer_d = self.optimizers()
 
-        fake_source_images = self.generator(target_images)
-
-        # Train discriminator
-        self.toggle_optimizer(optimizer_d)
-        # # how well can it label as real?
-        # valid = torch.ones(source_images.size(0), 1, 8, 8).type_as(source_images)
-        pred_real_source_images = self.discriminator(source_images)
-        real_loss = self.mse_loss(pred_real_source_images, torch.ones_like(pred_real_source_images))
-        # # how well can it label as fake?
-        # fake = torch.zeros(source_images.size(0), 1, 8, 8).type_as(source_images)
-        pred_fake_source_images = self.discriminator(fake_source_images.detach())
-        fake_loss = self.mse_loss(pred_fake_source_images, torch.zeros_like(pred_fake_source_images))
-        d_loss = real_loss + fake_loss
-        optimizer_d.zero_grad()
-        self.manual_backward(d_loss)
-        optimizer_d.step()
-        self.untoggle_optimizer(optimizer_d)
-
         # Train generator
         self.toggle_optimizer(optimizer_g)
-        # valid = torch.ones(source_images.size(0), 1, 8, 8).type_as(source_images)
+        fake_source_images = self.generator(target_images)
         pred_fake_source_images = self.discriminator(fake_source_images)
         g_loss = self.mse_loss(pred_fake_source_images, torch.ones_like(pred_fake_source_images))
         optimizer_g.zero_grad()
         self.manual_backward(g_loss)
         optimizer_g.step()
         self.untoggle_optimizer(optimizer_g)
+
+        # Train discriminator
+        self.toggle_optimizer(optimizer_d)
+        # how well can it label as real?
+        pred_real_source_images = self.discriminator(source_images)
+        real_loss = self.mse_loss(pred_real_source_images, torch.ones_like(pred_real_source_images))
+        # how well can it label as fake?
+        pred_fake_source_images = self.discriminator(fake_source_images.detach())
+        fake_loss = self.mse_loss(pred_fake_source_images, torch.zeros_like(pred_fake_source_images))
+        d_loss = 0.5 * (real_loss + fake_loss)
+        optimizer_d.zero_grad()
+        self.manual_backward(d_loss)
+        optimizer_d.step()
+        self.untoggle_optimizer(optimizer_d)
 
         log_freq = self.hparams.log_freq
 

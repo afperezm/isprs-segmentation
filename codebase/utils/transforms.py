63b6af59e133389587be32ee29af570037dea88b
Felipe Perez <perezmaf@myumanitoba.ca>
2024-04-23 02:06:37 -0500

    Convert single image to tensor

--- codebase/utils/transforms.py
+++ codebase/utils/transforms.py
@@ -6,12 +6,12 @@ class ToTensor(object):
     """Convert numpy arrays to tensors."""
 
     def __call__(self, batch):
-        source_image, target_image = batch
+        images = batch
 
         # Swap axis to place number of channels in front
-        source_image, target_image = np.transpose(source_image, (2, 0, 1)), np.transpose(target_image, (2, 0, 1))
+        images = np.transpose(images, (2, 0, 1))
 
         # Convert images to tensors
-        source_tensor, target_tensor = torch.from_numpy(source_image), torch.from_numpy(target_image)
+        images_tensor = torch.from_numpy(images)
 
-        return source_tensor, target_tensor
+        return images_tensor

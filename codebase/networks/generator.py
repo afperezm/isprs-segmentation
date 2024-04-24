50396b5e94bb50034a53ee75cd187200c24083a0
Felipe Perez <perezmaf@myumanitoba.ca>
2024-04-24 14:24:50 -0500

    Change tensor format from NCHW to NHWC to ease color indexing

--- codebase/networks/generator.py
+++ codebase/networks/generator.py
@@ -14,15 +14,21 @@ class ColorGANGenerator(nn.Module):
         nn.init.zeros_(self.bias)
 
     def forward(self, img):
-        img_r = (img[:, 0, :, :] + 1) * 127.5
-        img_g = (img[:, 1, :, :] + 1) * 127.5
-        img_b = (img[:, 2, :, :] + 1) * 127.5
+        img = torch.transpose(img, 1, 3)
+        img = torch.transpose(img, 1, 2)
+
+        img_r = (img[:, :, :, 0] + 1) * 127.5
+        img_g = (img[:, :, :, 1] + 1) * 127.5
+        img_b = (img[:, :, :, 2] + 1) * 127.5
 
         idx = img_r * 256 * 256 + img_g * 256 + img_b
         idx = idx.long()
 
         img_trans = self.weight[idx] * img + self.bias[idx]
         img_trans = torch.tanh(img_trans)
+
+        img_trans = torch.transpose(img_trans, 1, 3)
+        img_trans = torch.transpose(img_trans, 2, 3)
 
         return img_trans
 

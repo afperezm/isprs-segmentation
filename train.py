28ca69ff786413bea9c68cb1f325b3a12abe2394
Felipe Perez <perezmaf@myumanitoba.ca>
2024-05-06 18:14:12 -0500

    Use full extent of validation images

--- train.py
+++ train.py
@@ -71,7 +71,6 @@ def main():
 
         valid_dataset.dataset.transform = transforms.Compose([
             transforms.ToTensor(),
-            transforms.RandomCrop((224, 224)),
             transforms.Normalize([0.485, 0.456, 0.406, 0, 0, 0], [0.229, 0.224, 0.225, 1, 1, 1])
         ])
     else:

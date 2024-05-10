Fix normalization of pastis extracted images

utils/extract_pastis_dataset.py
@@ -27,7 +27,12 @@ def main():
 
         for time_idx in range(ts.shape[1]):
             image_name = f'S2_{ts_idx}_{time_idx:03d}.png'
-            image_array = ts[:, time_idx, 0:3, :, :].numpy().transpose(1, 2, 0)
+            image_tensor = ts[0, time_idx, 0:3, :, :]
+            image_array = image_tensor.numpy().transpose(1, 2, 0)
+            image_array = cv2.cvtColor(image_array + 0.5, cv2.COLOR_BGR2RGB)
+            image_array[image_array < 0.0] = 0.0
+            image_array[image_array > 1.0] = 1.0
+            image_array = (255 * image_array)
             print(image_name, image_array.shape, np.min(image_array), np.max(image_array))
             _ = cv2.imwrite(os.path.join(output_dir, image_name), image_array)
 
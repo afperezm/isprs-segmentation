a7089d1465158e0c3d894189b0a37724b5cf6094
Felipe Perez <perezmaf@myumanitoba.ca>
2024-05-10 12:12:07 -0500

    Apply min-max normalization to extracted pastis images

--- utils/extract_pastis_dataset.py
+++ utils/extract_pastis_dataset.py
@@ -29,10 +29,10 @@ def main():
             image_name = f'S2_{ts_idx}_{time_idx:03d}.png'
             image_tensor = ts[0, time_idx, 0:3, :, :]
             image_array = image_tensor.numpy().transpose(1, 2, 0)
-            image_array = cv2.cvtColor(image_array + 0.5, cv2.COLOR_BGR2RGB)
-            image_array[image_array < 0.0] = 0.0
-            image_array[image_array > 1.0] = 1.0
-            image_array = (255 * image_array)
+            image_array = cv2.cvtColor(image_array, cv2.COLOR_BGR2RGB)
+            mins = np.percentile(image_array, 0.0, axis=(0, 1), keepdims=True)
+            maxs = np.percentile(image_array, 100.0, axis=(0, 1), keepdims=True)
+            image_array = 255 * (image_array_rgb - mins) / (maxs - mins)
             print(image_name, image_array.shape, np.min(image_array), np.max(image_array))
             _ = cv2.imwrite(os.path.join(output_dir, image_name), image_array)
 
